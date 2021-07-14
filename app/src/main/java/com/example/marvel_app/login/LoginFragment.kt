85696e2fc3e028fb46d5250f2258e5c66020a3c2
Lukasz Fratczak <lukasz.fratczak03@gmail.com>
2021-07-14 08:09:34 +0200

    Correct displaying progress bar on login screens

--- app/src/main/java/com/example/marvel_app/login/LoginFragment.kt
+++ app/src/main/java/com/example/marvel_app/login/LoginFragment.kt
@@ -1,11 +1,11 @@
 package com.example.marvel_app.login
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
